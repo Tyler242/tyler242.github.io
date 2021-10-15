b35bd47d12d23650a04e86153805283b8a820f88
Tyler242 <she17007@byui.edu>
2021-10-14 22:40:07 -0600

    finished preston

--- lesson5/js/nav.js
+++ lesson5/js/nav.js
@@ -15,3 +15,26 @@ window.onresize = () => {
     nav.style.display = 'none';
   }
 };
+
+// Assuming that each page is built similarly, this should work the same on each page.
+
+// Get the html title element and html nav ul element.
+const currentPage = document.getElementsByTagName('title');
+const navUl = document.querySelector('#nav');
+
+// get the li elements inside the nav ul.
+const navLi = [...navUl.children];
+
+// get each a tag within each li tag.
+let navAElements = [];
+navLi.forEach((one) => {
+  navAElements.push(one.firstChild);
+});
+
+// iterate through the a tags until the innerHTML matches the id of the page.
+// Then apply the current class to the matching a tag.
+navAElements.forEach((el) => {
+  if (el.innerHTML == currentPage[0].id) {
+    el.className += ' current';
+  }
+});
