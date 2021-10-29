63870132088e50b22f9f5689eaf6719b0fc08dcf
Tyler242 <she17007@byui.edu>
2021-10-28 22:06:32 -0600

    changed picture to figure

--- lesson7/js/imageText.js
+++ lesson7/js/imageText.js
@@ -1,9 +1,9 @@
-// Get all the picture elements.
-const picElements = document.getElementsByTagName('picture');
-const picArray = [...picElements];
+// Get all the figure elements.
+const figElements = document.getElementsByTagName('figure');
+const figArray = [...figElements];
 
 // Get all the child elements that are images.
-picArray.forEach((element) => {
+figArray.forEach((element) => {
   element.children[1].innerHTML +=
     ' ' + element.children[0].getAttribute('alt');
 });
