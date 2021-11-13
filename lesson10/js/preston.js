4f25aafe1c56240571d48c65ee28b20cbbe7b343
Tyler242 <she17007@byui.edu>
2021-11-13 12:08:08 -0700

    Preston 10 complete

---
+++ lesson10/js/preston.js
@@ -0,0 +1,5 @@
+// Emergency Banner JS
+
+if (date.getDay() == 5) {
+  document.querySelector('#eBanner').style.display = 'block';
+}
---
+++ lesson10/js/weatherapi.js
@@ -0,0 +1,22 @@
+const apiURL =
+  'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=744fe90a723fbf1d7ad8053159c6a1d5';
+
+fetch(apiURL)
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      alert('Error with api call/response');
+    }
+  })
+  .then((jsObject) => {
+    let weatherData = jsObject.list[0];
+    document.getElementById('current-temp').textContent = weatherData.main.temp;
+
+    const imagesrc =
+      'https://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+    const desc = weatherData.weather[0].descritption;
+    document.getElementById('imagesrc').textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
