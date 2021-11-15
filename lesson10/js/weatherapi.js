b01d0c6fd8afafc46fd5507021e36dbc3858a763
Tyler242 <she17007@byui.edu>
2021-11-15 13:40:12 -0700

    api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,5 +1,5 @@
 const apiURL =
-  'http://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=744fe90a723fbf1d7ad8053159c6a1d5';
+  'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&appid=744fe90a723fbf1d7ad8053159c6a1d5';
 
 fetch(apiURL)
   .then((response) => {
@@ -14,7 +14,9 @@ fetch(apiURL)
     document.getElementById('current-temp').textContent = weatherData.main.temp;
 
     const imagesrc =
-      'https://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+      'https://openweathermap.org/img/w/' +
+      weatherData.weather[0].icon +
+      '.png';
     const desc = weatherData.weather[0].descritption;
     document.getElementById('imagesrc').textContent = imagesrc;
     document.getElementById('icon').setAttribute('src', imagesrc);
